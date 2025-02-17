ed414916ce73976a5dc410d878bea499a4ad62c9
DongWoonKim <01030985843z@naver.com>
2025-02-17 09:34:21 +0900

    update

--- src/main/java/com/example/spring/authservice/controller/UserApiController.java
+++ src/main/java/com/example/spring/authservice/controller/UserApiController.java
@@ -8,10 +8,7 @@ import com.example.spring.authservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -21,6 +18,11 @@ public class UserApiController {
 
     private final UserService userService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 
     @PostMapping("/join")
     public UserJoinResponseDTO join(@RequestBody UserJoinRequestDTO requestDTO) {
